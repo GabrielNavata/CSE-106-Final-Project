73edbeaa8ef72e50a973ae3fc972daa5eae40347
GabrielNavata <NavataGabriel@gmail.com>
2023-04-20 23:59:47 -0700

    updated db

--- ForumApp/models.py
+++ ForumApp/models.py
@@ -1,9 +1,23 @@
 #models
 from . import db
+from datetime import datetime
 from flask_login import UserMixin
-
 
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String(150))
+    password = db.Column(db.String(150), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('Users', backref=db.backref('posts', lazy=True))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
