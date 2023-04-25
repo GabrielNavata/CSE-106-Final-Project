42728e9ffd52ec8a41d95934625a117cca793254
GabrielNavata <NavataGabriel@gmail.com>
2023-04-25 00:09:02 -0700

    added view posts, comments, upvotes

--- ForumApp/models.py
+++ ForumApp/models.py
@@ -8,6 +8,7 @@ class Users(db.Model, UserMixin):
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
 
+    users_comment_vote = db.relationship('CommentVotes', backref='author', lazy=True)
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -16,8 +17,59 @@ class Posts(db.Model):
     title = db.Column(db.String(80), nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    views = db.Column(db.Integer, default=0)
+    category = db.Column(db.String(50), nullable=False)
+
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     user = db.relationship('Users', backref=db.backref('posts', lazy=True))
 
+    comments = db.relationship('Comments', backref='post', lazy=True)
+
+    def formatted_timestamp(self):
+        return self.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+
     def __repr__(self):
         return '<Post %r>' % self.title
+    
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('Users', backref=db.backref('comments', lazy=True))
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+
+    comment_votes = db.relationship('CommentVotes', backref='comment_votes', lazy=True)
+
+    def get_vote_counts(self):
+        upvotes = CommentVotes.query.filter_by(comment_id=self.id, vote=True).count()
+        downvotes = CommentVotes.query.filter_by(comment_id=self.id, vote=False).count()
+        return upvotes, downvotes
+    
+    def formatted_timestamp(self):
+        return self.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+class CommentVotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vote = db.Column(db.Boolean, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('Users', backref=db.backref('users_comment_votes', lazy=True))
+
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'), nullable=False)
+    comment = db.relationship('Comments', backref=db.backref('all_comment_votes', lazy=True))
+
+    def __repr__(self):
+        if self.vote == True:
+            vote = 'Up'
+        else:
+            vote = 'Down'
+        return '<Vote - {}, from {} for {}>'.format(vote, self.user.username)
+
+
+
+
--- ForumApp/views.py
+++ ForumApp/views.py
@@ -25,10 +25,51 @@ def browse():
 
 # Route for individual posts
 # change later to include the post id in the route
-@views.route('/post', methods=['GET', 'POST'])
-def post():
+@views.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def post(post_id):
+    post = Posts.query.get_or_404(post_id)
+    post.views += 1
+    db.session.commit()
 
-    return render_template('post.html')
+    if request.method == 'POST':
+        comment_content = request.form['comment']
+        new_comment = Comments(content=comment_content, post_id=post_id, user_id=current_user.id)
+        db.session.add(new_comment)
+        db.session.commit()
+
+ 
+
+    return render_template('post.html', post=post)
+
+@views.route('/votes/<int:post_id>/<int:comment_id>/<action>', methods=['GET', 'POST'])
+@login_required
+def comment_vote(post_id, comment_id, action):
+    # comment = Comments.query.get_or_404(comment_id)
+    comment = Comments.query.filter_by(id = comment_id).first_or_404()
+    votes = CommentVotes.query.filter_by(user = current_user, comment = comment).first()
+
+    # if action == 'like':
+    #     comment_like = CommentLikes(liked=True, user_id=current_user.id, comment_id=comment.id)
+    #     db.session.add(comment_like)
+    # else:
+    #     comment_like = CommentLikes.query.filter_by(user_id=current_user.id, comment_id=comment.id).first()
+    #     db.session.delete(comment_like)
+    if votes:
+        if votes.vote != bool(int(action)):
+            votes.vote = bool(int(action))
+            db.session.commit()
+            return redirect(url_for('views.post', post_id = post_id))
+        else:
+            flash('You already vote for this post')
+            return redirect(url_for('views.post', post_id = post_id))
+        
+    vote = CommentVotes(user=current_user, comment=comment, vote = bool(int(action)))
+    db.session.add(vote)
+    db.session.commit()
+    return redirect(url_for('views.post', post_id = post_id))
+
+
+
 
 # Route for creating posts
 @views.route('/create', methods=['GET', 'POST'])
@@ -38,8 +79,9 @@ def create():
         title = request.form['title']
         content = request.form['content']
         user_id = current_user.id
+        category = request.form['category']
         
-        post = Posts(title=title, content=content, user_id=user_id)
+        post = Posts(title=title, content=content, user_id=user_id, category=category)
         db.session.add(post)
         db.session.commit()
         
